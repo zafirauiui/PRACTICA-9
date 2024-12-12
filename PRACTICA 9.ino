c6b2f38e4ab93954f06db75464efc16656b871ef
zafirauiui <152870523+zafirauiui@users.noreply.github.com>
2024-12-12 10:21:11 +0100

    Update PRACTICA 9.ino
    ENCENDIDIO DE UN LED 13 CONECTADO AL PIN 13

--- PRACTICA 9.ino
+++ PRACTICA 9.ino
@@ -1,11 +1,20 @@
 /* Esta e unha practica de un LED no pin 13 de encender e apagar.
 Autor:Dolores Patiño
 Data:12/12/2024
-
-//define led 13 
-
-
+// C++ code
+//
 void setup()
-  pinMode(LED, OUTPUT) 
+{
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+}
 
-  serial
+void loop()
+{
+  digitalWrite(13, HIGH);
+  digitalWrite(12, LOW);
+  delay(10000); // Wait for 10000 millisecond(s)
+  digitalWrite(13, LOW);
+  digitalWrite(12, HIGH);
+  delay(4000); // Wait for 4000 millisecond(s)
+}
